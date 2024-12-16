b6df343ca2620f7fde9116e7cec1f4c8c0884a55
guillaume prof <guillaume.saintetienne@iter.org>
2024-12-16 18:35:33 +0100

    sum

--- AdeventOfCode.Tests/DistanceListTest.cs
+++ AdeventOfCode.Tests/DistanceListTest.cs
@@ -24,15 +24,37 @@ public class DistanceListTest
         
         actual.Should().BeEquivalentTo ( new List<int>(){2});
     }
+    
+    
+    [Fact]
+    public void GlobalDistanceBetween2Lists()
+    {
+        var list1 = new List<int> { 1 };
+        var list2 = new List<int> { 3 };
+        var actual = GlobalDistance(list1, list2);
+        
+        actual.Should().Be ( 2);
+    }
+    
+    
+    [Fact]
+    public void GlobalDistanceBetween2Lists2()
+    {
+        var list1 = new List<int> { 1, 3 };
+        var list2 = new List<int> { 3, 1 };
+        var actual = GlobalDistance(list1, list2);
+        
+        actual.Should().Be ( 4 );
+    }
+
+    private int GlobalDistance(List<int> list1, List<int> list2)
+    {
+        return Distance(list1, list2).Sum();
+        //.Aggregate((a, b) => a + b);
+    }
 
     private static List<int> Distance(List<int> list1, List<int> list2)
     {
-        var result = new List<int>();
-        for (var i = 0; i < list1.Count; i++)
-        {
-            result.Add(DistanceTest. Distance(list1[i], list2[i]));
-        } 
-
-        return result;
+        return list1.Select((value1, index) => DistanceTest.Distance(value1, list2[index])).ToList();
     }
 }
