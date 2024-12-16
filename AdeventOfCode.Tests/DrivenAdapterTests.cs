03d1cad26ed58d85d8555522437477aa7d032a02
guillaume prof <guillaume.saintetienne@iter.org>
2024-12-16 19:02:18 +0100

    adapter 1

---
+++ AdeventOfCode.Tests/DrivenAdapterTests.cs
@@ -0,0 +1,22 @@
+using System.Data.Common;
+using AdventOfCode.Src;
+using FluentAssertions;
+
+namespace AdeventOfCode.Tests;
+
+public class DrivenAdapterTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var input = "1 2";
+        var adapter = new Adapter();
+        var actual = adapter.ParseInput(input);
+            
+        actual.Should().Be( (1, 2));
+        actual.Item1.Should().Be(1);
+        actual.Item2.Should().Be(2);
+     
+    }
+    
+}
---
+++ AdventOfCode.Src/Adapter.cs
@@ -0,0 +1,9 @@
+namespace AdventOfCode.Src;
+
+public class Adapter
+{
+    public (int, int) ParseInput(string input)
+    {
+        return (1, 2);
+    }
+}
