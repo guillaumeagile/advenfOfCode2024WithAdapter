03194df41cc25468adafacde8ecf7b4e8f85780e
guillaume prof <guillaume.saintetienne@iter.org>
2024-12-16 18:45:48 +0100

    all business rules implemented 👏

--- AdeventOfCode.Tests/DistanceListTest.cs
+++ AdeventOfCode.Tests/DistanceListTest.cs
@@ -44,17 +44,47 @@ public class DistanceListTest
         var list2 = new List<int> { 3, 1 };
         var actual = GlobalDistance(list1, list2);
         
-        actual.Should().Be ( 4 );
+        actual.Should().Be ( 0 );
+    }
+    
+    
+    [Fact]
+    public void SortList()
+    {
+        var list1 = new List<int> { 3, 1, 2, 4 };
+        var actual = list1.SelfSort();
+        
+        actual.Should().BeEquivalentTo ( new List<int>(){1, 2, 3, 4});
+    }
+    
+    
+    [Fact]
+    public void GlobalDistanceBetween2ListsBug()
+    {
+        var list1 = new List<int> { 2, 1 };
+        var list2 = new List<int> { 1, 4 };
+        var actual = GlobalDistance(list1, list2);
+        
+        actual.Should().Be ( 2 );
     }
 
     private int GlobalDistance(List<int> list1, List<int> list2)
     {
-        return Distance(list1, list2).Sum();
+        return Distance(list1.SelfSort(), list2.SelfSort()).Sum();
         //.Aggregate((a, b) => a + b);
     }
 
     private static List<int> Distance(List<int> list1, List<int> list2)
     {
         return list1.Select((value1, index) => DistanceTest.Distance(value1, list2[index])).ToList();
+    }
+}
+
+
+static class Extentions
+{
+    public static List<T> SelfSort<T>(this List<T> list)
+    {
+        return list.OrderBy(x => x).ToList();
     }
 }
