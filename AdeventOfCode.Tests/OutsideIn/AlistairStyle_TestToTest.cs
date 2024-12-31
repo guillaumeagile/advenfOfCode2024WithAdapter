7ddd8840bfd44bd59fdc1e319c653af9c222b4c3
guillaume prof <guillaume.saintetienne@iter.org>
2024-12-31 18:22:34 +0100

    namespace AdventOfCode.Port4ProcessingData acts as a port for decoupling the driver from coreDomain implementation

--- AdeventOfCode.Tests/OutsideIn/AlistairStyle_TestToTest.cs
+++ AdeventOfCode.Tests/OutsideIn/AlistairStyle_TestToTest.cs
@@ -1,62 +1,75 @@
+using AdventOfCode.Port4ProcessingData;
 using AdventOfCode.Src;
 using FluentAssertions;
 
-namespace AdventOfCode.Tests.OutsideIn;
 
-public class AlistairStyle_TestToTest
+
+namespace AdventOfCode.Tests.OutsideIn
 {
-    [Fact]
-    public void Test1()
+    public class AlistairStyle_TestToTest
     {
-        var list1 = new List<int> { 2, 1 };
-        var list2 = new List<int> { 1, 4 };
-        var drivenReader = new DummyReader(list1, list2);
-        var dummyDriver = new DummyDriver(drivenReader);
+        [Fact]
+        public void Test1()
+        {
+            var list1 = new List<int> { 2, 1 };
+            var list2 = new List<int> { 1, 4 };
+            var drivenReader = new DummyReader(list1, list2);
+            var dummyDriver = new DummyDriver(drivenReader);
 
-        var actual = dummyDriver.DayOne();
-        // pseudo monkey patching
-        actual.Should().Be(2);
+            var actual = dummyDriver.DayOne();
+            // pseudo monkey patching
+            actual.Should().Be(2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var list1 = new List<int> { 3, 4, 2, 1, 3, 3 };
+            var list2 = new List<int> { 4, 3, 5, 3, 9, 3 };
+            var drivenReader = new DummyReader(list1, list2);
+            var dummyDriver = new DummyDriver(drivenReader);
+
+            var actual = dummyDriver.DayOne();
+            // pseudo monkey patching
+            actual.Should().Be(11);
+        }
     }
 
-    [Fact]
-    public void Test2()
+    public class DummyDriver(I4ExtractingColumns drivenReader) : I4ProcessingData
     {
-        var list1 = new List<int> { 3, 4, 2, 1, 3, 3 };
-        var list2 = new List<int> { 4, 3, 5, 3, 9, 3 };
-        var drivenReader = new DummyReader(list1, list2);
-        var dummyDriver = new DummyDriver(drivenReader);
+        public int DayOne()
+        {
+            return AdventOfCode.Port4ProcessingData.DomainImplementation.GlobalDistance(drivenReader.ExtractTwoColumns());
+        }
+    }
 
-        var actual = dummyDriver.DayOne();
-        // pseudo monkey patching
-        actual.Should().Be(11);
+    public class DummyReader(IEnumerable<int> list1, IEnumerable<int> list2) : I4ExtractingColumns
+    {
+        public (IEnumerable<int> l1, IEnumerable<int> l2) ExtractTwoColumns()
+        {
+            return (list1, list2);
+        }
+    }
+
+    public interface I4ExtractingColumns
+    {
+        (IEnumerable<int> l1, IEnumerable<int> l2) ExtractTwoColumns();
     }
 }
 
-public class DummyDriver(I4ExtractingColumns drivenReader) : I4ProcessingData
+namespace AdventOfCode.Port4ProcessingData
 {
-
-    public int DayOne()
-    {
-        return CoreDomain.GlobalDistance( drivenReader.ExtractTwoColumns());
-    }
-}
-
-public interface I4ProcessingData
-{
-    int DayOne();
     
-}
-
-public class DummyReader( IEnumerable<int> list1, IEnumerable<int> list2) : I4ExtractingColumns
-{
-    
-    public (IEnumerable<int> l1, IEnumerable<int> l2) ExtractTwoColumns()
+    public interface I4ProcessingData
     {
-        return (list1, list2);
+        int DayOne();
     }
-}
-
-public interface I4ExtractingColumns
-{
-    (IEnumerable<int> l1, IEnumerable<int> l2) ExtractTwoColumns();
+    
+    public static class DomainImplementation
+    {
+        public static int GlobalDistance((IEnumerable<int> l1, IEnumerable<int> l2) twoColumns)
+        {
+            return CoreDomain.GlobalDistance(twoColumns) ;
+        }
+    }
 }
--- AdventOfCode.Src/CoreDomain/CoreDomain.cs
+++ AdventOfCode.Src/CoreDomain/CoreDomain.cs
@@ -1,13 +1,13 @@
 ﻿namespace AdventOfCode.Src;
 
-public class CoreDomain 
+public static class CoreDomain
 {
     public static int GlobalDistance(IEnumerable<int> list1, IEnumerable<int> list2)
-     {
-         return Distance(list1.SelfSort(), list2.SelfSort()).Sum();
-         //.Sum est équivalent à .Aggregate((a, b) => a + b);
-     }
-    
+    {
+        return Distance(list1.SelfSort(), list2.SelfSort()).Sum();
+        //.Sum est équivalent à .Aggregate((a, b) => a + b);
+    }
+
     public static int Distance(int value1, int value2)
     {
         return int.Abs(value2 - value1);
@@ -18,7 +18,6 @@ public class CoreDomain
         return list1.Select((value1, index) => Distance(value1, list2.ElementAt(index))).ToList();
     }
 
- 
 
     public static int GlobalDistance((IEnumerable<int> l1, IEnumerable<int> l2) extractedTwoColumns)
     {
@@ -26,7 +25,7 @@ public class CoreDomain
     }
 }
 
-internal static class Extentions
+internal static class Extensions
 {
     public static List<T> SelfSort<T>(this IEnumerable<T> list)
     {
